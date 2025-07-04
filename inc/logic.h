14fd45fbd3530503c22db8761ed0b464f2c0450f
kolszewska16 <kingaolszewska16@gmail.com>
2025-07-04 13:27:18 +0200

    logic.h

--- inc/logic.h
+++ inc/logic.h
@@ -2,28 +2,31 @@
 #define LOGIC_H_
 
 #include <avr/io.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-typedef enum{
-	STATE_INPUT_AMOUNT,
-	STATE_SELECT_SOURCE_CURRENCY,
+typedef enum {
+	STATE_INPUT_AMOUNT,         
+	STATE_SELECT_SOURCE_CURRENCY, 
 	STATE_SELECT_TARGET_CURRENCY,
-	STATE_DISPLAY_RATE,
-	STATE_DISPLAY_RESULT
+	STATE_DISPLAY_RESULT       
 } AppState;
 
 extern AppState currentAppState;
-
-extern char inputBuffer[17];
-extern int inputBufferIndex;
+extern char inputBuffer[10];     
+extern uint8_t inputBufferIndex;
 extern double currentAmount;
 extern char sourceCurrency;
 extern char targetCurrency;
-extern char lastPressedCurrencyKey;
 
-void clearInputBuffer(void);
+static const double USD_TO_PLN_RATE = 3.60;
+static const double EURO_TO_PLN_RATE = 4.25;
+static const double GBP_TO_PLN_RATE = 4.92;
+static const double PLN_TO_PLN_RATE = 1.00;
+
 void resetConverter(void);
-void updateDisplay(void);
-double getExchangeRate(char from, char to);
-double convertCurrency(double amount, char src, char trg);
+double getExchangeRate(char src, char trg);
+double calculateConvertedAmount(double amount, char src, char trg);
+double getRateToPLN(char currency);
 
 #endif
